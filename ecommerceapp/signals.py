50b559cd077a6b48113a1982ed35fcdb40e79057
navamidr <navamidr@gmail.com>
2025-01-24 18:15:11 +0530

    fix:email validation

--- ecommerceapp/signals.py
+++ ecommerceapp/signals.py
@@ -3,6 +3,8 @@ from django.dispatch import receiver
 from .models import ContactQuery
 from django.conf import settings
 from .utils import notify_admin
+from django.core.mail import send_mail
+from django.conf import settings
 
 @receiver(post_save, sender=ContactQuery)
 def send_contact_email_to_admin(sender, instance, created, **kwargs):
@@ -20,4 +22,17 @@ def send_contact_email_to_admin(sender, instance, created, **kwargs):
             details=details,
             user_email=instance.email
         )
-       
+       # Notify the User
+        subject = "We Received Your Contact Query"
+        message = (
+            f"Hi {instance.name},\n\n"
+            f"Thank you for your Contact Query.\n\n"
+            f"Best regards,\nThe Support Team"
+        )
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,  # Use your project's default sender email
+            [instance.email],  # Recipient email
+            fail_silently=False,  # Raise an error if email sending fails
+        )
